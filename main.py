80618e676894167751634fdba678bb32a1461571
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-17 16:37:12 +0530

    Upadated main() function

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import CaesarCipher # Import the CaesarCipher Module
-while True:
-    print("\nCASER CIPHER")
-    print("1. Encrypt\n2. Decrypt\n3. Exit")
-    opt = int(input("Enter the index number corresponding your choice: ")) # Get user choice
 
-    if opt == 1:
-        PlainText = input("Enter the message to encrypt: ")
-        try:
-            Key = int(input("Enter the shift value (key): ")) # Get key (shift value)
-        except ValueError:
-            print("Invalid key. Please enter an Integer.")
-            continue # Skip to next iteration if key is not an integer
+def main():
+    while True:
+        print("\nCASER CIPHER")
+        print("1. Encrypt\n2. Decrypt\n3. Exit")
+        opt = int(input("Enter the index number corresponding your choice: ")) # Get user choice
 
-        CipherText = CaesarCipher.CCEncrypt(PlainText, Key)
-        # Print Encrypted/Cipher Message
-        print(f"Encrypted Message: %s" % CipherText)
-    
-    elif opt == 2:
-        CipherText = input("Enter the message to decrypt: ")
-        try:
-            Key = int(input("Enter the shift value (key): ")) # Get key (shift value)
-        except ValueError:
-            print("Invalid key. Please enter an integer.")
-            continue # Skip to next iteration if key is not an integer
+        if opt == 1:
+            PlainText = input("Enter the message to encrypt: ")
+            try:
+                Key = int(input("Enter the shift value (key): ")) # Get key (shift value)
+            except ValueError:
+                print("Invalid key. Please enter an Integer.")
+                continue # Skip to next iteration if key is not an integer
 
-        PlainText = CaesarCipher.CCDecrypt(CipherText, Key)
-        print(f"Decrypted Message: %s" % PlainText) # Print Decrypted/PlainText Message
-    
-    elif opt == 3:
-        break # Exit the loop on option 3
+            CipherText = CaesarCipher.CCEncrypt(PlainText, Key)
+            # Print Encrypted/Cipher Message
+            print(f"Encrypted Message: %s" % CipherText)
+        
+        elif opt == 2:
+            CipherText = input("Enter the message to decrypt: ")
+            try:
+                Key = int(input("Enter the shift value (key): ")) # Get key (shift value)
+            except ValueError:
+                print("Invalid key. Please enter an integer.")
+                continue # Skip to next iteration if key is not an integer
 
-    else:
-        print("Invalid choice. Try Again.")
+            PlainText = CaesarCipher.CCDecrypt(CipherText, Key)
+            print(f"Decrypted Message: %s" % PlainText) # Print Decrypted/PlainText Message
+        
+        elif opt == 3:
+            break # Exit the loop on option 3
+
+        else:
+            print("Invalid choice. Try Again.")
+
+if __name__ == "__main__":
+    main()
