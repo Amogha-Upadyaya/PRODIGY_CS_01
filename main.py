34bea12e5ff13b6b0f78b3f8f82999a1bf19866d
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-17 14:21:40 +0530

    Create main program for Caesar Cipher Algorithm

---
+++ main.py
@@ -0,0 +1,29 @@
+import CaesarCipher
+
+def main():
+    while True:
+        print("\nCASER CIPHER")
+        print("1. Encrypt\n2. Decrypt\n3. Exit")
+        opt = int(input("Enter the index number corresponding your choice: "))
+
+        if opt == 1:
+            PlainText = input("Enter the message to encrypt: ")
+            Key = int(input("Enter the shift value (key): "))
+
+            CipherText = CaesarCipher(PlainText, Key, mode="encrypt")
+            print(f"Encrypted Message: %s" % CipherText)
+        
+        elif opt == 2:
+            CipherText = input("Enter the message to decrypt: ")
+            Key = int(input("Enter the shift value (key): "))
+            PlainText = CaesarCipher(PlainText, Key, mode="decrypt")
+            print(f"Decrypted Message: %s" % PlainText)
+        
+        elif opt == 3:
+            break
+
+        else:
+            print("Invalid choice. Try Again.")
+
+if __name__ == "__main__":
+    main()
